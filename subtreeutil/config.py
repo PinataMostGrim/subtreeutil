dea69e40d93bee52ffba47bdfe7066974aacf3b5
Aaron <pinatamostgrim@gmail.com>
2019-05-15 17:56:33 -0300

    Add configuration validation and custom configuration errors

--- subtreeutil/config.py
+++ subtreeutil/config.py
@@ -26,6 +26,23 @@ _DEFAULT_CONFIG = {
 _loaded_config = None
 
 
+class ConfigurationError(Exception):
+    """Base error for configuration Errors.
+    """
+
+
+class InvalidConfigurationError(ConfigurationError):
+    """An error occurred while loading a configuration file.
+    """
+    pass
+
+
+class EmptyConfigurationError(ConfigurationError):
+    """An error occurred because configuration values were accessed before a configuration was loaded.
+    """
+    pass
+
+
 def edit_config_file(config_path: Path):
     """Opens the specified configuration file with the system's default editor.
     """
@@ -37,17 +54,16 @@ def edit_config_file(config_path: Path):
         create_config_file(config_path)
 
     try:
-        # Windows
+        # Open file in Windows
         os.startfile(str(config_path))
     except AttributeError:
-        # OSX / Linux
+        # Open file in OSX / Linux
         subprocess.Popen(['open', str(config_path)])
 
 
 def create_config_file(config_path: Path):
     """Creates a configuration file with default values at the specified path.
     """
-    # Try exception here?
     with config_path.open(mode='w') as f:
         json.dump(get_default_config(), f, indent=4)
 
@@ -60,15 +76,39 @@ def get_default_config():
 
 def load_config_file(config_path: Path):
     """Loads a configuration file into the loaded configuration global dictionary.
+
+    Raises:
+        FileNotFoundError: An error occured while attempting to load a configuration file.
+        InvalidConfigurationError: The specified configuration file is not valid.
     """
     global _loaded_config
 
-    # TODO: Try catch here
-    # TODO: Try except here?
-    with config_path.open('r') as f:
-        _loaded_config = json.load(f)
+    try:
+        with config_path.open('r') as f:
+            configuration = json.load(f)
+    except FileNotFoundError:
+        # TODO: Log event
+        raise FileNotFoundError
 
-    # TODO: Validate config file here
+    if validate_configuration(configuration):
+        _loaded_config = configuration
+    else:
+        raise InvalidConfigurationError
+
+
+def validate_configuration(configuration):
+    default_config = get_default_config()
+    is_valid_config = True
+
+    for key, value in default_config.items():
+        try:
+            configuration[key]
+        except KeyError:
+            # TODO: Log event instead of printing it
+            print(f'Configuration is missing key \'{key}\'')
+            is_valid_config = False
+
+    return is_valid_config
 
 
 def get_config():
@@ -76,11 +116,14 @@ def get_config():
 
     Returns:
         The global dictionary storing a mapping configuration keys to values.
+
+    Raises:
+        EmptyConfigurationError: A configuration value is accessed without a configuration file being loaded first.
     """
     global _loaded_config
     if _loaded_config is None:
-        return get_default_config()
         # TODO: Log event
+        raise EmptyConfigurationError
 
     return _loaded_config
 
