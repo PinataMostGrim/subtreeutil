b31389b1a13238577a341cde4f8a59b748c3e78e
Aaron <pinatamostgrim@gmail.com>
2019-05-16 20:53:22 -0300

    Adjust formatting to reduce length of perform_checkout() method

--- subtreeutil/core.py
+++ subtreeutil/core.py
@@ -32,35 +32,28 @@ def perform_checkout(config_path: Path):
     config.load_config_file(config_path)
 
     remote_name = config.get_remote_name()
-    remote_url = config.get_remote_url()
     branch = config.get_branch()
     source_paths = config.get_source_paths()
-    destination_paths = config.get_destination_paths()
-    cleanup_paths = config.get_cleanup_paths()
 
-    add_remote(remote_name, remote_url)
+    add_remote(remote_name, config.get_remote_url())
     fetch_remote(remote_name)
 
     commit_hash = get_remote_head_hash(remote_name, branch)
-
     core_log_display.info(f'\nChecking out files from {remote_name}/{branch} ({commit_hash})\n')
 
     for source_path in source_paths:
-        checkout_remote_source(
-            remote_name,
-            branch,
-            source_path)
+        checkout_remote_source(remote_name, branch, source_path)
 
     unstage_all()
     remove_remote(remote_name)
 
-    # Note: zip will stop as soon as the shortest list is exhausted.
-    for source_path, destination_path in zip(source_paths, destination_paths):
-        move_source(
-            Path(source_path),
-            Path(destination_path))
+    # Note: zip() will stop as soon as the shortest list is exhausted.
+    for source_path, destination_path in zip(
+            source_paths,
+            config.get_destination_paths()):
+        move_source(Path(source_path), Path(destination_path))
 
-    for cleanup_path in cleanup_paths:
+    for cleanup_path in config.get_cleanup_paths():
         cleanup_path = Path(cleanup_path)
         delete_source(cleanup_path)
 
