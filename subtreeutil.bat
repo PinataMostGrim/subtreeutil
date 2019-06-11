dd4c09b25c2aaa43fcb451ada006dabfa798b10f
Aaron <pinatamostgrim@gmail.com>
2019-06-10 21:36:28 -0300

    Fix execution from folders outside of the project folder
    In order for Python to recognize the module, its parent folder must be added to the PYTHONPATH environment variable. We use setlocal and endlocal in order to change the environment variable while the batch file is running.

--- subtreeutil.bat
+++ subtreeutil.bat
@@ -1,3 +1,9 @@
 @echo off
-REM: Run subtree_cli.py using the virtual environment '.venv'
+:: Run subtree module using its virtual environment interpreter.
+
+setlocal
+:: In order to support executing this batch from from any location,
+:: module's parent folder must be added to the Python path.
+set PYTHONPATH=%~dp0
 "%~dp0.venv\Scripts\python.exe" -m subtreeutil %*
+endlocal
